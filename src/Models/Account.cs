620c35a9634a6a2a75b2d787c36698afdfd909dc
Toon Langendam <t.langendam@gmail.com>
2022-01-09 19:13:12 +0100

    Account class

---
+++ src/Models/Account.cs
@@ -0,0 +1,27 @@
+public class Account
+{
+    public int Id {get; set;}
+    public string Voornaam {get; set;}
+    public string Achternaam {get; set;}
+    public string Adres {get; set;}
+    public string Woonplaats {get; set;}
+    public string Postcode {get; set;}
+    public string Email {get; set;}
+
+    public Account(string voornaam, string achternaam, string adres, string woonplaats, string postcode, string email)
+    {
+        Voornaam = voornaam;
+        Achternaam = achternaam;
+        Adres = adres;
+        Woonplaats = woonplaats;
+        Postcode = postcode;
+        Email = email;
+    }
+
+    public Account WijzigAccount(int Id, string voornaam, string achternaam, string adres, string woonplaats, string postcode, string email)
+    {
+        Account OriginalAccount = null;// where account id = id
+        Account ChangedAccount = OriginalAccount; // .edit() OR new Account()
+        return ChangedAccount;
+    }
+}
