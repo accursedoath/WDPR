010267a4ded754d10a55a3b1ca2a47db3d053d45
AccursedOath <atlasflamezz@outlook.com>
2022-01-13 10:47:56 +0100

    Chat update 1

--- src/Controllers/ChatController.cs
+++ src/Controllers/ChatController.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using src.Models;
 
@@ -14,14 +18,24 @@ namespace src.Controllers
     public class ChatController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DatabaseContext _context;
 
-        public ChatController(ILogger<HomeController> logger)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        private readonly UserManager<IdentityUser> _userManager;
+        public ChatController(ILogger<HomeController> logger, DatabaseContext context, IHttpContextAccessor httpContextAccessor, UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
+                var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                //var usa = _context.Account.Where
+                ViewBag.UserName = _context.Users.Where(x => x.Id == userId).Single().Email;
             return View();
         }
     }
