3c76fd479e65969315a89e4f7c3bef8339f909dd
Jordy <20169892@student.hhs.nl>
2022-01-22 14:01:34 +0100

    Update ClientController.cs

--- src/Controllers/ClientController.cs
+++ src/Controllers/ClientController.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Security.Claims;
-using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using Microsoft.AspNetCore.Identity;
 
 namespace src.Controllers
 {
