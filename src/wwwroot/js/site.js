02275e5e584cce6472092ce7d4acf0c0fde7ecfa
Jordy <20169892@student.hhs.nl>
2022-01-23 19:17:30 +0100

    tekst groter/kleiner knop

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function fontSizeLg() {
+    document.body.style.fontSize = "x-large";
+}
+
+function fontSizeMd() {
+    document.body.style.fontSize = "medium";
+}
